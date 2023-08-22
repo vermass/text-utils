2c5306a4a6f3ab9d7de91b82fed3608a31ce7d19
shail151192 <shailendra.kumar@zomato.com>
2023-08-22 21:08:50 +0530

    test utils

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String [] args) {
+        String s = "shailendra";
+        s = StringUtils.capitalize(s);
+        System.out.println("Hello world main !!" + s);
+    }
+}
---
+++ src/main/Utils.java
@@ -0,0 +1,10 @@
+package main;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Utils {
+
+    public static String capitalize(String name) {
+        return StringUtils.capitalize(name);
+    }
+}
